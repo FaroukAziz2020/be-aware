2508bc1169c820f140697a16132f2299dd23a4e6
Farouk Aziz <azizfarouk85@gmail.com>
2025-10-26 01:57:38 +0200

    removed Aptfile and build

--- backend/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-apt-get update
-apt-get install -y tesseract-ocr tesseract-ocr-eng tesseract-ocr-deu tesseract-ocr-fra tesseract-ocr-hun poppler-utils
-pip install -r requirements.txt
